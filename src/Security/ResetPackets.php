b08546dbd07a5107cfd320cd0061dafba3cbabc9
ByxNilsGFX <38230414+ByxNilsGFX@users.noreply.github.com>
2018-08-01 20:45:55 +0200

    Update ResetPackets.php

--- src/Security/ResetPackets.php
+++ src/Security/ResetPackets.php
@@ -3,22 +3,21 @@
 namespace Security;
 
 use pocketmine\plugin\Plugin;
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 
-class ResetPackets extends PluginTask{
+class ResetPackets extends Task{
     public $plugin;
 
     public function __construct(Plugin $owner) {
-        $this->plugin = $owner;
-        parent::__construct($owner);
+        $this->plugin = $owner
     }
 
     public function onRun(int $currentTick) {
         foreach ($this->plugin->packets as $count){
             $key = key($this->plugin->packets);
             if($count >= 150){
-                if($this->plugin->getServer()->getPlayer($key)){
-                    $this->plugin->getServer()->getPlayer($key)->kick("§4Surpassed packet limit!", false);
+                if($this->plugin->getPlayer($key)){
+                    $this->plugin->getPlayer($key)->kick("§4Surpassed packet limit!", false);
                 }
             }
             next($this->plugin->packets);
